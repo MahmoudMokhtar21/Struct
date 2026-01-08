414141a86c5999cc0b5e8bcbdd8e2110602443ad
Mahmoud-Mokhtar <mahmoudmoukhtar93@gmail.com>
2026-01-08 11:06:14 +0200

    initial

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    public struct first
+    {
+        // public int v;
+        public int MyProperty { get; set; }
+        public first(int x) { }
+        public void show()
+        {
+            Console.WriteLine("not show");
+        }
+        public void end()
+        {
+            Console.WriteLine("not show");
+        }
+
+    }
+
+
+    public struct Digitalsize
+    {
+        private long bit;
+
+        public string Byte => $"{(bit/bitsinbyte):N0}Bit";
+        public string KB => $"{(bit / bitsinkb):N0}KB";
+        public string MB => $"{(bit / bitsinmb):N0}MB";
+        public string GB => $"{(bit / bitsingb):N0}GB";
+        public string TB => $"{(bit / bitsintb):N0}TB";
+
+
+
+
+
+
+        private const long bitsinbits = 1;
+        private const long bitsinbyte = 8;
+        private const long bitsinkb = bitsinbyte * 1024;
+        private const long bitsinmb = bitsinkb * 1024;
+        private const long bitsingb = bitsinmb * 1024;
+        private const long bitsintb = bitsingb * 1024;
+        public Digitalsize(long initialvalue)
+            { 
+            this.bit = initialvalue;
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// struct support ->
+// events - indexers - opperator overloading - fields - constructor - preperties - methods
+//----------------------------------------------
+// struct implicity ingerites object base class but not support inhiretance
+// struct is value typy otherside class is refrence type
+// readonly exist only in struct nor class
+// struct doesnt support finalizers and inhiretances
+// new is mandatory(in one condietion u can not use new()
+
+using ConsoleApp13;
+
+//first f;  new() is mandatory but if there 's feilds and prperties have to use new()
+//f.show();
+first f = new first();
+f.show();
+
+
+Digitalsize size = new Digitalsize(1000000);
+Console.WriteLine(size.Byte);
+Console.WriteLine(size.KB);
+Console.WriteLine(size.MB);
+Console.WriteLine(size.GB);
+Console.WriteLine(size.TB);
+
